1c035b9d5765430ef19d85abbaef4bd6af9f73ef
MeDeity <546024423@qq.com>
2018-01-30 23:17:46 +0800

    (+)聊天界面UI及主界面UI

--- app/src/main/java/com/deity/flymessage/MainActivity.java
+++ app/src/main/java/com/deity/flymessage/MainActivity.java
@@ -1,13 +1,72 @@
 package com.deity.flymessage;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends Activity {
+
+    @BindView(R.id.beach_shui)
+    public ImageView beach_shui;
+
+    @BindView(R.id.beach_lang)
+    public ImageView beach_lang;
+
+    @BindView(R.id.bottle_getting)
+    public ImageView bottle_getting;
+
+    @BindView(R.id.bottle_sending)
+    public ImageView bottle_sending;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initViews();
+    }
+
+    private void initViews(){
+        beach_shui.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, R.anim.anim_shake));
+        beach_lang.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, R.anim.anim_shake));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        String userStr = "用户名:%s,appKey:%s,版本号:%s";
+//        UserInfo info = JMessageClient.getMyInfo();
+//        tv_user_info.setText(String.format(userStr,info.getUserName(),info.getAppKey(),JMessageClient.getSdkVersionString()));
+    }
+
+    @SuppressWarnings("unused")
+    @OnClick(R.id.send_nav_image)
+    public void sendBottleMsg(View view){
+        new AlertDialog.Builder(MainActivity.this).setView(R.layout.dialog_bottle_send)
+                .show();
+//        bottle_sending.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, R.anim.anim_move_up));
+    }
+
+    @SuppressWarnings("unused")
+    @OnClick(R.id.get_nav_image)
+    public void getBottleMsg(View view){
+        new AlertDialog.Builder(MainActivity.this).setView(R.layout.dialog_bottle_receiver)
+                .show();
+//        bottle_getting.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, R.anim.anim_move_up));
+    }
+
+
+    @SuppressWarnings("unused")
+    @OnClick(R.id.item_bottle_msg)
+    public void getBottle(View view){
+        startActivity(new Intent(MainActivity.this,ChatActivity.class));
     }
 }
--- app/src/main/java/com/deity/flymessage/data/FlyMessageApplication.java
+++ app/src/main/java/com/deity/flymessage/data/FlyMessageApplication.java
@@ -1,6 +1,7 @@
 package com.deity.flymessage.data;
 
 import android.app.Application;
+import android.util.DisplayMetrics;
 import android.util.Log;
 
 import cn.jpush.im.android.api.JMessageClient;
@@ -12,6 +13,19 @@ import cn.jpush.im.android.api.JMessageClient;
 
 public class FlyMessageApplication extends Application {
 
+    /**
+     * 屏幕宽度
+     */
+    public static int screenWidth;
+    /**
+     * 屏幕高度
+     */
+    public static int screenHeight;
+    /**
+     * 屏幕密度
+     */
+    public static float screenDensity;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -19,5 +33,15 @@ public class FlyMessageApplication extends Application {
         Log.i("IMDebugApplication", "init");
         JMessageClient.setDebugMode(true);
         JMessageClient.init(getApplicationContext(), true);
+    }
+
+    /**
+     * 初始化当前设备屏幕宽高
+     */
+    private void initScreenSize() {
+        DisplayMetrics curMetrics = getApplicationContext().getResources().getDisplayMetrics();
+        screenWidth = curMetrics.widthPixels;
+        screenHeight = curMetrics.heightPixels;
+        screenDensity = curMetrics.density;
     }
 }
